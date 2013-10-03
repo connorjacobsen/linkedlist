b76990a6b72273e13205af669c478920a3e00198
Connor A. Jacobsen <cjacobse@lin113-20.cise.ufl.edu>
2013-10-03 15:27:26 -0400

    Defined the base Node and LL structs without any methods

--- ll.h
+++ ll.h
@@ -1,5 +1,20 @@
 #include <iostream>
 
 /*
- * This header contains 
+ * This header contains the structs and method declarations for
+ * a linked list and node library implementation. Code inspired
+ * by Dave Small's COP3503 class.
  */
+
+// The Node struct. Currently it uses an int data type.
+struct Node 
+{
+  int data;     // the data that the Node holds
+  Node* next;   // pointer to the next Node in the list
+}
+
+// The LinkedList struct.
+struct LinkedList
+{
+  Node* head;   // points to the first Node in the list
+}
